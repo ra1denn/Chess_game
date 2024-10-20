66e598e8b4cbaf5d596973ad97f43687b1372bed
Maria <biwlolsmeuduo@gmail.com>
2024-10-20 16:20:29 +0300

    Initial commit

---
+++ Assets/Scripts/Pieces/Piece.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public enum PieceType {King, Queen, Bishop, Knight, Rook, Pawn};
+
+public abstract class Piece : MonoBehaviour
+{
+    public PieceType type;
+
+    protected Vector2Int[] RookDirections = {Vector2Int.up, Vector2Int.right, Vector2Int.down, Vector2Int.left};
+    protected Vector2Int[] BishopDirections = {Vector2Int.right + Vector2Int.up, Vector2Int.right + Vector2Int.down, Vector2Int.left + Vector2Int.down, Vector2Int.left + Vector2Int.up};
+
+    public abstract List<Vector2Int> MoveLocations(Vector2Int gridPoint);
+}
